2bc6d8ca17ad48f7f2aacf603161b31f41685833
Yuliia Danylenko <yvdanilenko@gmail.com>
2022-02-01 23:44:09 +0200

    dependency installation

---
+++ src/Components/App/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = { good: 0, neutral: 0, bad: 0 };
+  render() {
+    return <div> HELLO </div>;
+  }
+}
+
+export default App;
